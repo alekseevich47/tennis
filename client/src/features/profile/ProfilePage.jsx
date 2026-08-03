be349c3c976267d922929b80d99662a46e561b47
alekseevich47 <kkabenyuk@gmail.com>
2026-08-03 19:59:55 +0700

    v0.38

--- client/src/features/profile/ProfilePage.jsx
+++ client/src/features/profile/ProfilePage.jsx
@@ -24,6 +24,7 @@ import {
   isMobileMaxPlatform,
   openSellerChat
 } from '../shop/buyMessage';
+import cardIconUrl from '../../assets/card.png';
 import AboutAppModal from './AboutAppModal';
 import MembershipModal from './MembershipModal';
 import ProfileSingleDateField from './ProfileSingleDateField';
@@ -341,9 +342,14 @@ function ProfilePage({
           className="membership-btn"
           onClick={() => setMembershipOpen(true)}
         >
-          <svg viewBox="0 0 24 24" fill="#007aff" aria-hidden="true">
-            <path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20a2 2 0 002 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zm0-12H5V6h14v2zm-7 5h5v5h-5v-5z" />
-          </svg>
+          <img
+            src={cardIconUrl}
+            alt=""
+            className="membership-btn__icon"
+            width={24}
+            height={24}
+            aria-hidden="true"
+          />
         </IconButton>
       )}
 
